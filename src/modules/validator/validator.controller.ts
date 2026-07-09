ed2e858d5b07d2d1ef3ec7c7add7b37b1be3bd85
CharmCapsule <mondjoe007@yahoo.com>
2026-07-09 19:13:11 +0400

    Update validator.controller.ts

--- src/modules/validator/validator.controller.ts
+++ src/modules/validator/validator.controller.ts
@@ -1,8 +1,3 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param } from '@nestjs/common';
-import { ValidatorService } from './validator.service';
-import { CreateValidatorDto } from './dto/create-validator.dto';
-import { UpdateValidatorDto } from './dto/update-validator.dto';
-
 @Controller('validators')
 export class ValidatorController {
   constructor(private validatorService: ValidatorService) {}
